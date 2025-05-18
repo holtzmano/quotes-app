3836a8e0045684e61d9f212c1834cee849e2d93f
Oren Holtzman <holtzmanoren99@gmail.com>
2025-05-18 22:47:39 +0300

    g

--- backend/db.py
+++ backend/db.py
@@ -1,5 +1,9 @@
 from pymongo import MongoClient
 
+import os
+from dotenv import load_dotenv
+
 def get_db():
-    client = MongoClient("mongodb://localhost:27017")
+    uri = os.getenv('MONGO_URI', 'mongodb://localhost:27017')
+    client = MongoClient(uri)
     return client["quotesdb"]
